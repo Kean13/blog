03f902a0c21fc494ec63850e29e83382c9ebdfce
Kean <kean@octo.gg>
2024-08-09 03:09:03 +0500

    now navbar is optional component for all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 import Post from '@/components/app-post'
 import Header from '@/components/header/header-header'
+import Nav from '@/components/nav/nav-nav'
 
 import ExampleImage from '@/dev/example.png'
 
 export default function HomePage() {
 	return (
-		<main className='container'>
-			<Header />
-			<Post
-				image={ExampleImage}
-				date='Today at 10:00'
-				tag='Most Popular'
-				title='Lorem Ipsum'
-				description='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-				views='1.2K'
-				likes='64'
-			/>
-		</main>
+		<div>
+			<Nav />
+			<main className='container'>
+				<Header />
+				<Post
+					image={ExampleImage}
+					date='Today at 10:00'
+					tag='Most Popular'
+					title='Lorem Ipsum'
+					description='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+					views='1.2K'
+					likes='64'
+				/>
+			</main>
+		</div>
 	)
 }
